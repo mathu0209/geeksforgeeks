43c1cc88b34abe10de415276032b339daf5b1ed1
Mathu <mathu020906@gmail.com>
2026-03-11 13:52:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.java
@@ -0,0 +1,29 @@
+
+class Solution {
+    public int celebrity(int mat[][]) {
+        // code here
+        int n=mat.length;
+        Stack<Integer> st = new Stack<>();
+        for(int i=0; i<n; i++){
+            st.push(i);
+        }
+        while(st.size()>1){
+            int a = st.pop();
+            int b = st.pop();
+            if(mat[a][b] ==1){
+                st.push(b);
+            }
+            else{
+                st.push(a);
+            }
+        }
+        int people = st.pop();
+        for(int i=0; i<n; i++){
+            if(i ==people) continue;
+                  if(mat[i][people] !=1 || mat[people][i] !=0){
+                return -1;
+                  }
+        }
+        return people;
+    }
+}
